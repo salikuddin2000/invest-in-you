0420d99ab85c4ac1eb6f44567e1abbb62dc1455a
salik uddin <salikuddin2000@gmail.com>
2022-04-10 12:55:46 +0530

    shadow-1

--- components/dashboardComponents/Recommendations.js
+++ components/dashboardComponents/Recommendations.js
@@ -1,33 +1,43 @@
-import { Card, CardContent, Typography, Box, Container, CardActionArea } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Box,
+  Container,
+  CardActionArea,
+} from "@mui/material";
 import { textAlign } from "@mui/system";
-import { getProjectByDocIdRealTime, getProjectsForDashboard } from "../../backend/project";
+import {
+  getProjectByDocIdRealTime,
+  getProjectsForDashboard,
+} from "../../backend/project";
 import { useState } from "react";
-import { makeStyles } from '@mui/styles';
+import { makeStyles } from "@mui/styles";
 import { useEffect } from "react";
 import { async } from "@firebase/util";
 import Link from "next/link";
-
-
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 310,
     transition: "transform 0.15s ease-in-out",
     "&:hover": { transform: "scale3d(1.05, 1.05, 1)" },
-  }
+  },
 });
 
 export default function Recommendations() {
-  const {recommendationsList} = getProjectsForDashboard()
+  const { recommendationsList } = getProjectsForDashboard();
   const classes = useStyles();
   useEffect(() => {
-    console.log(recommendationsList)
+    console.log(recommendationsList);
   }, [recommendationsList]);
-  if (recommendationsList!==undefined)
+  if (recommendationsList !== undefined)
     return (
       //   <Box display="flex" justifyContent="center" alignItems="center" marginLeft="10%" marginRight="10%">
-      <Container style={{ paddingLeft: "10%", paddingRight: "10%", paddingTop: "20px" }}>
-        <Typography variant="h4" component="div" marginBottom="10px" >
+      <Container
+        style={{ paddingLeft: "10%", paddingRight: "10%", paddingTop: "20px" }}
+      >
+        <Typography variant="h4" component="div" marginBottom="10px">
           Recommendations
           <br />
         </Typography>
@@ -40,36 +50,52 @@ export default function Recommendations() {
               width: "25%",
               marginLeft: "4%",
               marginRight: "4%",
-              marginBottom: "10px"
+              marginBottom: "10px",
+              borderRadius: "16px",
+              background: "linear-gradient(145deg, #e6e6e6, #ffffff)",
+              boxShadow: "16px 16px 33px #e8e8e8,-16px -16px 33px #ffffff",
             }}
             className={classes.root}
-          >            
+          >
             <CardActionArea>
-            <Link
-                  href={{
-                    pathname:
-                      "/" + obj.assetId + "/" + obj.projectId,
-                    query: {
-                      projectRef: obj.projectId,
-                    },
-                  }}
-                  as={`/${obj.assetId}/${obj.projectId}`}
-                >
-              <CardContent>
-                <Typography variant="h5" style={{ fontWeight: "bolder" }}>
-                  {obj.projectName}
-                </Typography>
-                <Typography color="text.secondary" sx={{ fontWeight: '700', marginBottom: "15px" }}>{obj.assetName}</Typography>
-                <Typography color="text.secondary" style={{ fontSize: "10px" }} sx={{ fontWeight: '650' }}>
-                  Current Price
-                </Typography>
-                <Typography sx={{ fontWeight: 'bold', fontSize: 20 }} color={"green"}>₹{obj.currentPrice}</Typography>
-              </CardContent>
+              <Link
+                href={{
+                  pathname: "/" + obj.assetId + "/" + obj.projectId,
+                  query: {
+                    projectRef: obj.projectId,
+                  },
+                }}
+                as={`/${obj.assetId}/${obj.projectId}`}
+              >
+                <CardContent>
+                  <Typography variant="h5" style={{ fontWeight: "bolder" }}>
+                    {obj.projectName}
+                  </Typography>
+                  <Typography
+                    color="text.secondary"
+                    sx={{ fontWeight: "700", marginBottom: "15px" }}
+                  >
+                    {obj.assetName}
+                  </Typography>
+                  <Typography
+                    color="text.secondary"
+                    style={{ fontSize: "10px" }}
+                    sx={{ fontWeight: "650" }}
+                  >
+                    Current Price
+                  </Typography>
+                  <Typography
+                    sx={{ fontWeight: "bold", fontSize: 20 }}
+                    color={"green"}
+                  >
+                    ₹{obj.currentPrice}
+                  </Typography>
+                </CardContent>
               </Link>
             </CardActionArea>
           </Card>
         ))}
       </Container>
     );
-  else return(<h1>Loading</h1>);
+  else return <h1>Loading</h1>;
 }
